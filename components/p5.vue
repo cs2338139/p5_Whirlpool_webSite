a8a446f02a6a6a6cc640d1cd71880d968cdb03a4
JinCheng <cs2338139@gmail.com>
2023-04-21 11:15:40 +0800

    enter

--- components/p5.vue
+++ components/p5.vue
@@ -7,39 +7,48 @@ let sketch = null;
 onMounted(() => {
   sketch = new p5((p) => {
     class ball {
-      constructor(x, y, r, _color) {
+      constructor(x, y, r, _color, speed) {
         this.x = x;
         this.y = y;
         this.r = r;
         this._color = _color;
+        this.speed = speed;
       }
       a = 0;
       xArray = [];
       yArray = [];
+      aArray = [];
       update() {
         for (let i = 0; i < ballShadowCount; i++) {
-          if (this.xArray.length != 6) {
-            let _x = this.x - (p.windowWidth / 2 - this.x) - (i * (p.windowWidth / 2 - this.x)) / ((ballSpeed / 100) * ballShadowRate);
+          if (this.xArray.length != ballShadowCount) {
+            let _x = this.x - (p.windowWidth / 2 - this.x) - ((i * (p.windowWidth / 2 - this.x)) / this.speed / 100) * ballShadowRate;
             this.xArray.push(_x);
           } else {
-            this.xArray[i] -= (p.windowWidth / 2 - this.xArray[i]) / ballSpeed;
+            this.xArray[i] -= ((p.windowWidth / 2 - this.xArray[i]) / 1000) * this.speed;
           }
 
-          if (this.yArray.length != 6) {
-            let _y = this.y - (p.windowHeight / 2 - this.y) - (i * (p.windowHeight / 2 - this.y)) / ((ballSpeed / 100) * ballShadowRate);
+          if (this.yArray.length != ballShadowCount) {
+            let _y = this.y - (p.windowHeight / 2 - this.y) - ((i * (p.windowHeight / 2 - this.y)) / this.speed / 100) * ballShadowRate;
             this.yArray.push(_y);
           } else {
-            this.yArray[i] -= (p.windowHeight / 2 - this.yArray[i]) / ballSpeed;
+            this.yArray[i] -= ((p.windowHeight / 2 - this.yArray[i]) / 1000) * this.speed;
           }
+
+          if (this.aArray.length != ballShadowCount) {
+            this.aArray.push(0);
+          } else {
+            if (this.aArray[i] < this.a + (255 / this.xArray.length) * i) this.aArray[i] += 1;
+          }
+          // this._color.setAlpha(this.a + (255 / this.xArray.length) * i);
         }
 
-        this.r += 0.03;
+        if (this.r < ballMaxR) this.r += 0.04;
       }
 
       display() {
         for (let i = 0; i < this.xArray.length; i++) {
           p.push();
-          this._color.setAlpha(this.a + (255 / this.xArray.length) * i);
+          this._color.setAlpha(this.aArray[i]);
           p.fill(this._color);
           p.noStroke();
           p.ellipse(this.xArray[i], this.yArray[i], this.r);
@@ -48,13 +57,17 @@ onMounted(() => {
       }
     }
 
-    let ballCountMax = 10;
+    let ballCountMax = 500;
     let balls = new Array(ballCountMax);
     let ballShadowCount = 6;
-    let ballSpeed = 100;
-    let ballShadowRate = 30;
+    let ballMinSpeed = 8;
+    let ballMaxSpeed = 20;
+    let ballShadowRate = 20;
     let deviation = 120;
-    let _color = [p.color(235, 189, 52), p.color(72, 197, 232), p.color(240, 109, 58)];
+    let ballOneTimeAppear = 3;
+    let ballMaxR = 25;
+    let movePos = 0;
+    let _color = [p.color(235, 189, 52), p.color(72, 197                    , 232), p.color(240, 109, 58)];
 
     p.setup = () => {
       p.createCanvas(p.windowWidth, p.windowHeight).parent(container.value);
@@ -66,29 +79,40 @@ onMounted(() => {
 
     p.draw = () => {
       p.background(10);
-      let x, y;
+      let _ballOneTimeAppear = 0;
       for (let i = 0; i < ballCountMax; i++) {
         if (!balls[i]) {
-          x = p.randomGaussian() * deviation + p.windowWidth / 2;
-          y = p.randomGaussian() * deviation + p.windowHeight / 2;
-          // balls.push(new ball(x, y, p.random(1, 2), p.random(_color)));
-          balls[i] = new ball(x, y, p.random(1, 2), p.random(_color));
-          // console.log(balls);
-          break;
+          let x, y;
+          do {
+            x = p.randomGaussian() * deviation + p.windowWidth / 2;
+            y = p.randomGaussian() * deviation + p.windowHeight / 2;
+          } while (p.dist(p.windowWidth / 2, p.windowHeight / 2, x, y) < 20 || p.dist(p.windowWidth / 2, p.windowHeight / 2, x, y) > 30);
+
+          balls[i] = new ball(x, y, p.random(1, 2), p.random(_color), p.random(ballMinSpeed, ballMaxSpeed));
+          _ballOneTimeAppear++;
+          if (_ballOneTimeAppear >= ballOneTimeAppear) break;
         }
       }
-      let lastShadow = ballCountMax - 1;
+      let lastShadow = ballShadowCount - 1;
       for (let i = 0; i <= balls.length; i++) {
         if (balls[i] != null) {
           balls[i].update();
           balls[i].display();
-          if (balls[i].xArray[lastShadow] + balls[i].r < 0 || balls[i].xArray[lastShadow] - balls[i].r > p.windowWidth || balls[i].yArray[lastShadow] + balls[i].r < 0 || balls[i].yArray[lastShadow] - balls[i].r > p.windowHeight) {
+          let lastBallShadow_width_Left = balls[i].xArray[lastShadow] + balls[i].r;
+          let lastBallShadow_width_Right = balls[i].xArray[lastShadow] - balls[i].r;
+          let lastBallShadow_Height_Up = balls[i].yArray[lastShadow] + balls[i].r;
+          let lastBallShadow_Height_Down = balls[i].yArray[lastShadow] - balls[i].r;
+
+          if (lastBallShadow_width_Left < 0 || lastBallShadow_width_Right > p.windowWidth || lastBallShadow_Height_Up < 0 || lastBallShadow_Height_Down > p.windowHeight) {
             balls[i] = null;
-            console.log(balls[i]);
           }
-          console.log(balls[i].xArray[lastShadow] + balls[i].r < 0 + "_" + (balls[i].xArray[lastShadow] - balls[i].r) > p.windowWidth + "_" + (balls[i].yArray[lastShadow] + balls[i].r) < 0 + "_" + (balls[i].yArray[lastShadow] - balls[i].r) > p.windowHeight);
         }
       }
+    };
+
+    p.mouseWheel = (event) => {
+      movePos += event.delta / 1000;
+      console.log(movePos);
     };
   });
 });
