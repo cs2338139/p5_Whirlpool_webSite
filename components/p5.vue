0ab0c9564e1f41a49e450e921e155e630aad49bd
cs2338139 <64711409+cs2338139@users.noreply.github.com>
2023-05-08 00:42:40 +0800

    tass static balls (Bugs)

--- components/p5.vue
+++ components/p5.vue
@@ -19,22 +19,22 @@ onMounted(() => {
       yArray = [];
       aArray = [];
       update() {
-        for (let i = 0; i < ballShadowCount; i++) {
-          if (this.xArray.length != ballShadowCount) {
-            let _x = this.x - (p.windowWidth / 2 - this.x) - ((i * (p.windowWidth / 2 - this.x)) / this.speed / 100) * ballShadowRate;
+        for (let i = 0; i < dynamicBallShadowCount; i++) {
+          if (this.xArray.length != dynamicBallShadowCount) {
+            let _x = this.x - (p.windowWidth / 2 - this.x) - ((i * (p.windowWidth / 2 - this.x)) / this.speed / 100) * dynamicBallShadowRate;
             this.xArray.push(_x);
           } else {
             this.xArray[i] -= ((p.windowWidth / 2 - this.xArray[i]) / 1000) * this.speed;
           }
 
-          if (this.yArray.length != ballShadowCount) {
-            let _y = this.y - (p.windowHeight / 2 - this.y) - ((i * (p.windowHeight / 2 - this.y)) / this.speed / 100) * ballShadowRate;
+          if (this.yArray.length != dynamicBallShadowCount) {
+            let _y = this.y - (p.windowHeight / 2 - this.y) - ((i * (p.windowHeight / 2 - this.y)) / this.speed / 100) * dynamicBallShadowRate;
             this.yArray.push(_y);
           } else {
             this.yArray[i] -= ((p.windowHeight / 2 - this.yArray[i]) / 1000) * this.speed;
           }
 
-          if (this.aArray.length != ballShadowCount) {
+          if (this.aArray.length != dynamicBallShadowCount) {
             this.aArray.push(0);
           } else {
             if (this.aArray[i] < this.a + (255 / this.xArray.length) * i) this.aArray[i] += 1;
@@ -42,7 +42,7 @@ onMounted(() => {
           // this._color.setAlpha(this.a + (255 / this.xArray.length) * i);
         }
 
-        if (this.r < ballMaxR) this.r += 0.04;
+        if (this.r < dynamicBallMaxR) this.r += 0.04;
       }
 
       display() {
@@ -57,17 +57,20 @@ onMounted(() => {
       }
     }
 
-    let ballCountMax = 500;
-    let balls = new Array(ballCountMax);
-    let ballShadowCount = 6;
-    let ballMinSpeed = 8;
-    let ballMaxSpeed = 20;
-    let ballShadowRate = 20;
+    let staticBallsCountMax = 500;
+    let staticBalls = new Array(staticBallsCountMax);
+    let dynamicBallCountMax = 500;
+    let dynamicBalls = new Array(dynamicBallCountMax);
+    let dynamicBallShadowCount = 6;
+    let dynamicBallMinSpeed = 8;
+    let dynamicBallMaxSpeed = 20;
+    let dynamicBallShadowRate = 20;
     let deviation = 120;
-    let ballOneTimeAppear = 3;
-    let ballMaxR = 25;
+    let dynamicBallOneTimeAppear = 3;
+    let dynamicBallMaxR = 25;
     let movePos = 0;
-    let _color = [p.color(235, 189, 52), p.color(72, 197                    , 232), p.color(240, 109, 58)];
+    let _color = [p.color(235, 189, 52), p.color(72, 197, 232), p.color(240, 109, 58)];
+    let _staticColor = [p.color(235, 109, 52), p.color(2, 197, 232), p.color(240, 0, 58)];
 
     p.setup = () => {
       p.createCanvas(p.windowWidth, p.windowHeight).parent(container.value);
@@ -79,36 +82,66 @@ onMounted(() => {
 
     p.draw = () => {
       p.background(10);
+      CreateStaticBalls();
+      CreateDynamicBalls();
+      DynamicBallsUpdate();
+    };
+
+    function CreateStaticBalls() {
+      for (let i = 0; i < staticBallsCountMax; i++) {
+        let x, y;
+        do {
+          x = p.randomGaussian() * deviation + p.windowWidth / 2;
+          y = p.randomGaussian() * deviation + p.windowHeight / 2;
+        } while (p.dist(p.windowWidth / 2, p.windowHeight / 2, x, y) < 20 || p.dist(p.windowWidth / 2, p.windowHeight / 2, x, y) > 30);
+
+        staticBalls[i] = new ball(x, y, p.random(1, 2), p.random(_staticColor), p.random(dynamicBallMinSpeed, dynamicBallMaxSpeed));
+        staticBalls[i].display();
+        //FIXME
+      }
+    }
+
+    function CreateDynamicBalls() {
       let _ballOneTimeAppear = 0;
-      for (let i = 0; i < ballCountMax; i++) {
-        if (!balls[i]) {
+      for (let i = 0; i < dynamicBallCountMax; i++) {
+        if (!dynamicBalls[i]) {
           let x, y;
           do {
             x = p.randomGaussian() * deviation + p.windowWidth / 2;
             y = p.randomGaussian() * deviation + p.windowHeight / 2;
           } while (p.dist(p.windowWidth / 2, p.windowHeight / 2, x, y) < 20 || p.dist(p.windowWidth / 2, p.windowHeight / 2, x, y) > 30);
 
-          balls[i] = new ball(x, y, p.random(1, 2), p.random(_color), p.random(ballMinSpeed, ballMaxSpeed));
+          dynamicBalls[i] = new ball(x, y, p.random(1, 2), p.random(_color), p.random(dynamicBallMinSpeed, dynamicBallMaxSpeed));
           _ballOneTimeAppear++;
-          if (_ballOneTimeAppear >= ballOneTimeAppear) break;
+          if (_ballOneTimeAppear >= dynamicBallOneTimeAppear) break;
         }
       }
-      let lastShadow = ballShadowCount - 1;
-      for (let i = 0; i <= balls.length; i++) {
-        if (balls[i] != null) {
-          balls[i].update();
-          balls[i].display();
-          let lastBallShadow_width_Left = balls[i].xArray[lastShadow] + balls[i].r;
-          let lastBallShadow_width_Right = balls[i].xArray[lastShadow] - balls[i].r;
-          let lastBallShadow_Height_Up = balls[i].yArray[lastShadow] + balls[i].r;
-          let lastBallShadow_Height_Down = balls[i].yArray[lastShadow] - balls[i].r;
+    }
 
-          if (lastBallShadow_width_Left < 0 || lastBallShadow_width_Right > p.windowWidth || lastBallShadow_Height_Up < 0 || lastBallShadow_Height_Down > p.windowHeight) {
-            balls[i] = null;
-          }
+    function DynamicBallsUpdate() {
+      let lastShadow = dynamicBallShadowCount - 1;
+      for (let i = 0; i <= dynamicBalls.length; i++) {
+        if (dynamicBalls[i] != null) {
+          dynamicBalls[i].update();
+          dynamicBalls[i].display();
+          if (CheckDynamicBallOutSide(dynamicBalls[i], lastShadow)) dynamicBalls[i] = null;
         }
       }
-    };
+    }
+
+    function CheckDynamicBallOutSide(ball, lastShadow) {
+      let isOutSide = false;
+      let lastBallShadow_width_Left = ball.xArray[lastShadow] + ball.r;
+      let lastBallShadow_width_Right = ball.xArray[lastShadow] - ball.r;
+      let lastBallShadow_Height_Up = ball.yArray[lastShadow] + ball.r;
+      let lastBallShadow_Height_Down = ball.yArray[lastShadow] - ball.r;
+
+      if (lastBallShadow_width_Left < 0 || lastBallShadow_width_Right > p.windowWidth || lastBallShadow_Height_Up < 0 || lastBallShadow_Height_Down > p.windowHeight) {
+        isOutSide = true;
+      }
+
+      return isOutSide;
+    }
 
     p.mouseWheel = (event) => {
       movePos += event.delta / 1000;
